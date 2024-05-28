13ad54c4ef125116cf279fa0dbe7d4fa90a63d7c
Bruno Ce <brunoce2003@gmail.com>
2024-05-28 16:54:13 -0300

    Funcionalidades y estilos agregados

--- main.js
+++ main.js
@@ -7,24 +7,57 @@ const app = Vue.createApp({
                 estado: '',
                 puntaje: ''
             },
-            videojuegos: []
+            videojuegos: [],
+            filtro: {
+                nombre: '',
+                plataforma: '',
+                estado: ''
+            },
+            errores: []
+        }
+    },
+    computed: {
+        videojuegosFiltrados() {
+            return this.videojuegos.filter(videojuego => {
+                return (
+                    (this.filtro.nombre === '' || videojuego.nombre.toLowerCase().includes(this.filtro.nombre.toLowerCase())) &&
+                    (this.filtro.plataforma === '' || videojuego.plataforma === this.filtro.plataforma) &&
+                    (this.filtro.estado === '' || videojuego.estado === this.filtro.estado)
+                );
+            });
         }
     },
     methods: {
         registrarVideojuego() {
-            if (this.nuevoVideojuego.nombre &&
-                this.nuevoVideojuego.plataforma &&
-                this.nuevoVideojuego.estado &&
-                this.nuevoVideojuego.puntaje) {
+            this.errores = [];
+            if (this.comprobar()) {
                 this.videojuegos.push({...this.nuevoVideojuego});
                 console.log('Videojuego agregado:', this.videojuegos);
                 this.nuevoVideojuego.nombre = '';
                 this.nuevoVideojuego.plataforma = '';
                 this.nuevoVideojuego.estado = '';
                 this.nuevoVideojuego.puntaje = '';
-            } else {
-                alert('Por favor, completa todos los campos.');
             }
+        },
+        comprobar() {
+            let comprobado = true;
+            if (this.nuevoVideojuego.nombre === '') {
+                this.errores.push("El nombre no puede estar vacío.");
+                comprobado = false;
+            }
+            if (this.nuevoVideojuego.plataforma === '') {
+                this.errores.push("Debe seleccionar una plataforma.");
+                comprobado = false;
+            }
+            if (this.nuevoVideojuego.estado === '') {
+                this.errores.push("Debe seleccionar un estado.");
+                comprobado = false;
+            }
+            if (this.nuevoVideojuego.puntaje === '') {
+                this.errores.push("Debe asignar un puntaje.");
+                comprobado = false;
+            }
+            return comprobado;
         }
     }
 });
